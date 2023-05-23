31ea5cf56c13bc2a37165ead19eec127baa99b64
Renzo Rouma <renzo.rouma@gmail.com>
2023-05-23 22:05:11 +0200

    lazy load logo

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,7 @@
 
 <!-- Header -->
 <div class="header">
-    <img src="../images/Viovyx logo.png" class="pixel-image-block header" width="60px"/>
+    <img src="../images/Viovyx logo.png" class="pixel-image-block header" width="60px"/ loading="lazy">
     <div class="pixel-text-block header header-links">
         <a class="header-title" href="/">Viovyx</a>
         <a class="header" href="/tests">test page</a>
