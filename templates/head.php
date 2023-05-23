7b92b9f83f70b4ec814ce83e7dbba62af5cf2912
Renzo Rouma <renzo.rouma@gmail.com>
2023-05-23 22:13:51 +0200

    fix head.php
    css files were wrong in head.php for other pages

--- templates/head.php
+++ templates/head.php
@@ -1,12 +1,12 @@
 <meta charset="UTF-8">
 <title>Viovyx</title>
 <link rel="icon" type="image/png" href="images/favicon.png" />
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-<link href="css/fonts.css" rel="stylesheet" type="text/css" />
+<link href="/css/style.css" rel="stylesheet" type="text/css" />
+<link href="/css/fonts.css" rel="stylesheet" type="text/css" />
 
 <!-- Header -->
 <div class="header">
-    <img src="../images/Viovyx logo.png" class="pixel-image-block header" width="60px"/ loading="lazy">
+    <img src="/images/Viovyx logo.png" class="pixel-image-block header" width="60px" loading="lazy"/>
     <div class="pixel-text-block header header-links">
         <a class="header-title" href="/">Viovyx</a>
         <a class="header" href="/tests">test page</a>
