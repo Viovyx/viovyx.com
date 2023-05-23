c410e9a229e93de6cbfbe659a60146a70b97fc13
Renzo Rouma <renzo.rouma@gmail.com>
2023-05-23 14:22:46 +0200

    Site header
    added site header place holder and cleaned up the code a bit

---
+++ templates/tests.php
@@ -0,0 +1,20 @@
+<!-- Tests for fonts -->
+<h1 class="uni">This font is un 05_53.</h1>
+<h1 class="mono-reg">This font is dm mono regular.</h1>
+<h1 class="mono-ita">This font is dm mono italic.</h1>
+<h1 class="mono-light-ita">This font is dm mono light italic.</h1>
+<h1 class="mono-med">This font is dm mono medium.</h1>
+<h1 class="mono-med-ita">This font is dm mono medium italic.</h1>
+        
+<!-- Button test -->
+<button class="button-box">button test</button>
+
+<!-- Pixel text block shadow test -->
+<div class="pixel-text-block">
+    <h1 class="uni">Pixel text block test.</h1>
+</div>
+
+<!-- Pixel image block test -->
+<div class="pixel-image-block">
+    <img src="images\Viovyx logo.png" width="300px" />
+</div>
