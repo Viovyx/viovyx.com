6c12880b29bd8e991fef2e84587fc7aec05f5111
Renzo Rouma <renzo.rouma@gmail.com>
2023-07-04 14:08:28 +0200

    fixed social buttons width
    made the button width responsive depending on screen width

--- socials/index.php
+++ socials/index.php
@@ -8,9 +8,11 @@
     <body>
         <div class="body">
             <h1 class="body-text">My Socials:</h1>
-            <a class="button-box" href="https://www.twitch.tv/viovyx" target="_blank">twitch</a>
-            <a class="button-box" href="https://www.tiktok.com/@viovyx" target="_blank">tiktok</a>
-            <a class="button-box" href="https://www.youtube.com/@viovyx" target="_blank">youtube</a>
+            <div class="social-buttons">
+                <a class="button-box" href="https://www.twitch.tv/viovyx" target="_blank">twitch</a>
+                <a class="button-box" href="https://www.tiktok.com/@viovyx" target="_blank">tiktok</a>
+                <a class="button-box" href="https://www.youtube.com/@viovyx" target="_blank">youtube</a>
+            </div>
         </div>
     </body>
     
