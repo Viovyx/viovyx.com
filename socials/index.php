353fe0ca2c4e5bbe2795c1a316fbf00ccccb65f5
Renzo Rouma <renzo.rouma@gmail.com>
2023-07-26 17:31:59 +0200

    removed unnecessary social links
    removed: discord and steam

--- socials/index.php
+++ socials/index.php
@@ -13,8 +13,6 @@
                 <a class="button-box" href="https://www.tiktok.com/@viovyx" target="_blank">tiktok</a>
                 <a class="button-box" href="https://www.youtube.com/@viovyx" target="_blank">youtube</a>
                 <a class="button-box" href="https://github.com/Viovyx" target="_blank">github</a>
-                <a class="button-box" href="https://steamcommunity.com/id/Viovyx/" target="_blank">steam</a>
-                <a class="button-box" href="https://discordapp.com/users/431093546561568779" target="_blank">discord</a>
                 <a class="button-box" href="https://scoresaber.com/u/76561199206204757" target="_blank">scoresaber</a>
                 <a class="button-box" href="https://www.beatleader.xyz/u/76561199206204757" target="_blank">beatleader</a>
             </div>
