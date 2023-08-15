1a3a51d13a0cd50fe1e43128d634d73d9854d29c
Renzo Rouma <renzo.rouma@gmail.com>
2023-08-15 23:48:32 +0200

    change project status naming
    changed done to public and dropped to private, removed wip since it would be the same as private

--- projects/index.php
+++ projects/index.php
@@ -8,10 +8,11 @@
     <body>
         <div class="body">
             <h1 class="body-text">My Projects:</h1>
-            <p class="body-text">Don't be suprised if you see a lot of projects that never get finished.</p>
+            <p class="body-text">Don't be suprised if you see a lot of projects that never get published.</p>
+            
             <h2 class="project-title">Beat Saber Maps</h2>
             <div class="project-canvas">
-                <!-- Done -->
+                <!-- public -->
                 <a href="https://beatsaver.com/maps/34dd4" target="_blank"class="project-item">
                     <img src="/images/bs_maps/out-of-my-league.jpg" width="300px" height="auto">
                     <p class="project-item-text" id="song-time" >3:30</p>
@@ -19,7 +20,7 @@
                         <h2 class="project-item-text" id="song-title" >Out of My League</h2>
                         <h3 class="project-item-text" id="song-artist" >Fitz and The Tantrums</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
 
                 <a href="https://beatsaver.com/maps/" target="_blank" class="project-item">
@@ -29,7 +30,7 @@
                         <h2 class="project-item-text" id="song-title" >Pinguïndans</h2>
                         <h3 class="project-item-text" id="song-artist" >Schiffie & Co</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
 
                 <a href="https://beatsaver.com/maps/33902" target="_blank" class="project-item">
@@ -39,10 +40,10 @@
                         <h2 class="project-item-text" id="song-title" >Hate.</h2>
                         <h3 class="project-item-text" id="song-artist" >ThxSoMch</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
 
-                <!-- wip -->
+                <!-- private -->
                 <a href="https://open.spotify.com/track/0zo4aX57COch7Wh3NRImTM?si=a4c93e483a154156" target="_blank"class="project-item">
                     <img src="/images/bs_maps/heartbeat.jpg" width="300px" height="auto">
                     <p class="project-item-text" id="song-time" >4:31</p>
@@ -50,7 +51,7 @@
                         <h2 class="project-item-text" id="song-title" >Heartbeat</h2>
                         <h3 class="project-item-text" id="song-artist" >Childish Gambino</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-wip"></span></h4>
+                    <h4 class="project-status">private <span class="status-private"></span></h4>
                 </a>
 
                 <a href="https://open.spotify.com/track/5i1ckaduJ5lMO5wRawsuMp?si=9c741f47de144be5" target="_blank" class="project-item">
@@ -60,7 +61,7 @@
                         <h2 class="project-item-text" id="song-title" >Ava</h2>
                         <h3 class="project-item-text" id="song-artist" >Famy</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-wip"></span></h4>
+                    <h4 class="project-status">private <span class="status-private"></span></h4>
                 </a>
 
                 <a href="https://open.spotify.com/track/4mn5HdatHKN7iFGDes9G8i?si=d294dd7a71964aaf" target="_blank" class="project-item">
@@ -70,40 +71,19 @@
                         <h2 class="project-item-text" id="song-title" >Who's Ready for Tomorrow</h2>
                         <h3 class="project-item-text" id="song-artist" >RAT BOY, IBDY</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-wip"></span></h4>
+                    <h4 class="project-status">private <span class="status-private"></span></h4>
                 </a>
-
-                <!-- Dropped
-                <a href="https://open.spotify.com/track/1lpd8zLCBXv1a4uddGSfvC?si=df486236022b46e4" target="_blank" class="project-item">
-                    <img src="/images/bs_maps/Kiss You.jpg" width="300px" height="auto">
-                    <p class="project-item-text" id="song-time" >2:40</p>
-                    <div class="song-info">
-                        <h2 class="project-item-text" id="song-title" >Kiss You</h2>
-                        <h3 class="project-item-text" id="song-artist" >Nightcore Berlin, Nicco & Josh</h3>
-                    </div>
-                    <h4 class="project-status">status <span class="status-dropped"></span></h4>
-                </a>
-
-                <a href="https://open.spotify.com/track/6W947Bfx5AixTRA6a0dorj?si=5562e51d4b424b58" target="_blank" class="project-item">
-                    <img src="/images/bs_maps/VTUBERS.jpg" width="300px" height="auto">
-                    <p class="project-item-text" id="song-time" >2:45</p>
-                    <div class="song-info">
-                        <h2 class="project-item-text" id="song-title" >VTUBERS</h2>
-                        <h3 class="project-item-text" id="song-artist" >Constera</h3>
-                    </div>
-                    <h4 class="project-status">status <span class="status-dropped"></span></h4>
-                </a> -->
             </div>
 
             <h2 class="project-title">Wallpaper Engine</h2>
             <div class="project-canvas">
-                <!-- Done -->
+                <!-- public -->
                 <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3018766025" target="_blank" class="project-item">
                     <img src="/images/wallpapers/yayoi-forest.gif" width="300px" height="auto">
                     <div class="song-info">
                         <h2 class="project-item-text" id="wallpaper-title" >Yayoi Forest</h2>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
 
                 <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3018762115" target="_blank" class="project-item">
@@ -111,7 +91,7 @@
                     <div class="song-info">
                         <h2 class="project-item-text" id="wallpaper-title" >Moonlight Solitude</h2>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
 
                 <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3018751934" target="_blank" class="project-item">
@@ -119,7 +99,7 @@
                     <div class="song-info">
                         <h2 class="project-item-text" id="wallpaper-title" >Ship in Clouds</h2>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
 
                 <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=3018743799" target="_blank" class="project-item">
@@ -127,20 +107,20 @@
                     <div class="song-info">
                         <h2 class="project-item-text" id="wallpaper-title" >Amber Glade</h2>
                     </div>
-                    <h4 class="project-status">status <span class="status-done"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
             </div>
 
             <h2 class="project-title">Websites</h2>
             <div class="project-canvas">
-                <!-- wip -->
+                <!-- public -->
                 <a href="https://github.com/Viovyx/viovyx.com" target="_blank" class="project-item">
                     <img src="/images/Viovyx logo.png" width="300px" height="auto">
                     <div class="song-info">
                         <h2 class="project-item-text" id="website-title" >viovyx.com</h2>
                         <h3 class="project-item-text" id="website-description" >My main website that you're on right now.</h3>
                     </div>
-                    <h4 class="project-status">status <span class="status-wip"></span></h4>
+                    <h4 class="project-status">public <span class="status-public"></span></h4>
                 </a>
             </div>
         </div>
